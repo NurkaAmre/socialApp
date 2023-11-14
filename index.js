9cd040e3e5e937cffbd17028c8583e718ee5644c
NurkaAmre <nurkerey30@gmail.com>
2023-11-14 17:18:48 +0600

    Created routes for users & authorization

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -15,11 +17,18 @@ mongoose
   })
   .then(() => {
     console.log('Connected to MongoDB!');
-    // Your application logic after connecting
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is ready! ');
