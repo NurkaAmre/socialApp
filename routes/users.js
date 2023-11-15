9f6d74d6d224d23a5a7b51b70adb2ec7174f7b7d
NurkaAmre <nurkerey30@gmail.com>
2023-11-15 16:50:48 +0600

    User delete process

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,18 @@ router.put('/:id', async (req, res) => {
   }
  });
 //DELETE USER
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json('Account has been deleted sucessfully');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('You can delete only your account!');
+  }
+});
 //GET USER
 //FOLLOW USER
 //UNFOLLOW USER
