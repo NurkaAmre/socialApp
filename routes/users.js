df86e5afc491c388d41f6782906898600bcfd4d3
NurkaAmre <nurkerey30@gmail.com>
2023-11-15 17:01:08 +0600

    Get user

--- routes/users.js
+++ routes/users.js
@@ -39,6 +39,15 @@ router.delete('/:id', async (req, res) => {
   }
 });
 //GET USER
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //FOLLOW USER
 //UNFOLLOW USER
 
