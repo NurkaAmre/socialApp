1bc5ac209f2931c75e264cfdb79a43751b0c5b0d
NurkaAmre <nurkerey30@gmail.com>
2023-11-15 12:50:32 +0600

    remore err console

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,7 @@ router.post('/register', async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
   }
 });
 
@@ -27,13 +27,16 @@ router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(404).send('user not found');
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
     !validPassword && res.status(400).json('wrong password');
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
   }
- })
+});
 
 
 module.exports = router;
