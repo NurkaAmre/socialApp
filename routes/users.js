b715f5b18c8d656e0fefbfcde7389862ea6c78f7
NurkaAmre <nurkerey30@gmail.com>
2023-11-15 17:58:58 +0600

    User unfollow process

--- routes/users.js
+++ routes/users.js
@@ -68,7 +68,28 @@ router.put('/:id/follow', async (req, res) => {
     res.status(403).json('You cant follow yourself');
   }
 });
+
 //UNFOLLOW USER
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followins: req.params.id } });
+        res.status(200).json('User has been unfollowed');
+      } else {
+        res.status(403).json('You allready unfollowed this user');
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You cant unfollow yourself');
+  }
+});
+
 
 router.get('/', (req, res) => {
   res.send('Hey its user route');
