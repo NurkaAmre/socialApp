9ed89ea20abe9c2334150c285757531886690156
NurkaAmre <nurkerey30@gmail.com>
2023-11-15 12:13:27 +0600

    Add bycrypt for password

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('Connected to MongoDB!');
--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,18 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 //REGISTER
-router.get('/register', async (req, res) => {
+router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     password: req.body.password,
   });
+
   try {
-    const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
+    const user = await newUser.save();
+    res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    console.log(err);
   }
 });
 
