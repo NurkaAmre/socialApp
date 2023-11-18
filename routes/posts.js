4805ea0411f7b34cd1068e9951ed98431dfd5552
NurkaAmre <nurkerey30@gmail.com>
2023-11-18 12:18:42 +0600

    Create POST, uodate POST

--- routes/posts.js
+++ routes/posts.js
@@ -1,3 +1,43 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
+
+//CREATE POST
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE POST
+router.put('/:id', async (req, res) => { 
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('The post has been updated');
+    } else {
+      res.status(403).json('You can update only your post');
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  } 
+})
+
+//DELETE POST
+router.delete('/:id', async (req, res) => {
+
+})
+
+//LIKE/DISLIKE POST
+
+//GET POST
+
+//GET TIMELINE POSTS
+
+  
 
 module.exports = router;
